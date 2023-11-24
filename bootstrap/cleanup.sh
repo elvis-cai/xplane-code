705d7bda65b90ae0e8fadb2442f0f460ded2517d
elvis <elvis.cai@airwallex.com>
2023-11-24 14:44:48 +1100

    up

--- bootstrap/cleanup.sh
+++ bootstrap/cleanup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export KUBECONFIG=/tmp/awx-code-challenge.config
 kubectl delete application cloud-resources   -n argocd
 kubectl delete managed --all
 k3d cluster delete awx-code-challenge
